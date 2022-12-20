835aab5cc7aae39c8e5ca30ff94f9ae200064279
Jun Yuan <yjun@zjuici.com>
2022-12-20 19:00:13 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    "http://localhost:1234",
+]
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/greeting")
+async def greeting(name: str):
+    return "Hello " + name + "!"
